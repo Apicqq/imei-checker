7d3fa07307332b4a1496879bd876a2990a34e7aa
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-01-30 14:50:48 +0300

    feat: remove magic string

--- api_app/services.py
+++ api_app/services.py
@@ -1,14 +1,16 @@
-import aiohttp
+from http import HTTPStatus
 
+import aiohttp
 from fastapi.exceptions import HTTPException
 
 from api_app.schemas import ImeiIn
+from core.settings import settings
 from utils.imei_verification import check_imei
 
 async def verify_imei(body: ImeiIn, session: aiohttp.ClientSession) -> dict:
-    if body.token != "132fdsfsd":
+    if body.token != settings.api_token:
         raise HTTPException(
-            401,
+            HTTPStatus.UNAUTHORIZED,
             "Invalid API token"
         )
     return await check_imei(body.imei, session)
