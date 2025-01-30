16e31634be9cc1635ab8f8c37522644c5d41ddb5
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2025-01-30 17:30:27 +0300

    feat: add telegram bot settings to config

--- core/settings.py
+++ core/settings.py
@@ -1,15 +1,23 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class TelegramBotSettings(BaseModel):
+    """Class which contains all settings for telegram bot."""
+
+    telegram_bot_token: str = "Your_telegram_bot_token"
+    white_list_user_ids: str = "Your_white_list_user_ids, separated by comma"
 
 
 class Settings(BaseSettings):
     """Class which contains all the app settings."""
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
     api_token: str = "your_api_token"
-    white_list_user_ids: list[int] = None
     imeicheck_url: str = "https://api.imeicheck.net/v1/checks"
     imeicheck_token: str = "your_imeicheck_token"
     imeicheck_service_id: int = 12
+    bot_config: TelegramBotSettings = TelegramBotSettings()
 
 
 settings = Settings()
